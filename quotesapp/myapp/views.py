2c67957e3900827d7044b44b73751afcdbdf3478
Nikita Goldashevsky <nikitagoldashevsky@yandex.ru>
2025-09-07 11:31:26 +0300

    Add random quote view, template, URL

--- quotesapp/myapp/views.py
+++ quotesapp/myapp/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Quote
+import random
 
-# Create your views here.
+def random_quote(request):
+    quotes = list(Quote.objects.all())
+    if quotes:
+        # выбор цитаты с учетом веса
+        weights = [q.weight for q in quotes]
+        quote = random.choices(quotes, weights=weights, k=1)[0]
+        # увеличение счетчика просмотров
+        quote.views += 1
+        quote.save()
+    else:
+        quote = None
+
+    return render(request, 'myapp/random_quote.html', {'quote': quote})
